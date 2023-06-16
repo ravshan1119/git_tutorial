0e327fb6653b419d3e21f5673dada4cc3e823710
nurmuxammad <nurmuhammadzoyidov@gmail.com>
2023-06-16 19:44:35 +0500

    Nurmuxammad

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Column(children: [],),
+      home: const Column(children: [
+
+        Text("Nurmuxammad Zoyidov"),
+
+      ],),
     );
   }
 }
