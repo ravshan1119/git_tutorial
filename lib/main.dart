d71b4afc3fca24de1129e13c75a2b08a9e0caf36
nurmuxammad <nurmuhammadzoyidov@gmail.com>
2023-06-16 23:14:53 +0500

    Files added

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-
-      home: Column(children: [
-        Text("Nurmuxammad Zoyidov"),
-        Text("Abdulaziz"),
-        Text("Tursunov"),
-        Text("Sharif "), 
-        Text("___RAVSHAN___",style:  TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w500,
-          color: Colors.cyan
-        ),)],),
-
-
-    );
+    return const Placeholder();
   }
 }
