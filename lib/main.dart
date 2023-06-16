989ca8b18ce7fa42b5d979bf6a7705a2e12aeaa7
nurmuxammad <nurmuhammadzoyidov@gmail.com>
2023-06-16 23:56:15 +0500

    globall back button

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:git_tutorial/ui/home/screen_1.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp( const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+    );
   }
 }
