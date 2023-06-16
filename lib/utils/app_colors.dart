3e6cc82c508c47ef29c64e85ea4bb55c9267f3e4
Hp Victus <tursunovAbd563@gmail.com>
2023-06-17 00:27:46 +0500

    I added fonts, colors, text_theme

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,5 +1,36 @@
 
 
+import 'dart:ui';
+
 class AppColors{
+
+  static const Color C_000000 = Color(0xFF000000);
+  static const Color C_194B38 = Color(0xFF194B38);
+  static const Color C_2AAF7F = Color(0xFF2AAF7F);
+
+
+  static const Color C_4CBB5E = Color(0xFF4CBB5E);
+  static const Color C_26AD71 = Color(0xFF26AD71);
+
+
+  static const Color C_32CB4B = Color(0xFF32CB4B);
+  static const Color C_4B4B4B = Color(0xFF4B4B4B);
+
+
+  static const Color C_EC534A  = Color(0xFFEC534A );
+  static const Color C_EC6E47  = Color(0xFFEC6E47 );
+
+
+  static const Color C_F0C334 = Color(0xFFF0C334);
+  static const Color C_777777 = Color(0xFF777777);
+
+  static const Color C_9C9C9C = Color(0xFF9C9C9C);
+  static const Color C_E2E2E2 = Color(0xFFE2E2E2);
+  static const Color C_F5F5F5 = Color(0xFFF5F5F5);
+  static const Color C_1F4F3 = Color(0xFF1F4F3);
+  static const Color C_FAFFFD = Color(0xFFFAFFFD);
+  static const Color C_FFFFFF = Color(0xFFFFFFFF);
+
+
 
 }
