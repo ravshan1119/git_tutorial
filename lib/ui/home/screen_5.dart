767302dde5df5c68447560fc88c4f182fbe2d9b5
nurmuxammad <nurmuhammadzoyidov@gmail.com>
2023-06-17 14:08:58 +0500

    check

---
+++ lib/ui/home/screen_5.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FifthScreen extends StatefulWidget {
+  const FifthScreen({super.key});
+
+  @override
+  State<FifthScreen> createState() => _FifthScreenState();
+}
+
+class _FifthScreenState extends State<FifthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
