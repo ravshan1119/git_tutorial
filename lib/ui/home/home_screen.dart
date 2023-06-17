9703c96c072c2e4b0c7a161e710bf82255b0d1eb
nurmuxammad <nurmuhammadzoyidov@gmail.com>
2023-06-17 19:36:36 +0500

    Check Screen Buttons added to home screen

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:git_tutorial/ui/home/widgets/back_button.dart';
+import 'package:git_tutorial/ui/screen_5/screen_5.dart';
+
+import '../screen_4/show_modal_bottom_sheet.dart';
+
+class AllScreenButton extends StatefulWidget {
+  const AllScreenButton({super.key});
+
+  @override
+  State<AllScreenButton> createState() => _AllScreenButtonState();
+}
+
+class _AllScreenButtonState extends State<AllScreenButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+        title: const Text(
+          "Check Screen",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            Button(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return FifthScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 50,),
+            Button(
+              onPressed: () {},
+            ),
+            SizedBox(height: 50,),
+            Button(
+              onPressed: () {},
+            ),
+            SizedBox(height: 50,),
+            Button(
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
