b981ad2cff78dc3e7cefa3c2cb0cb2c42ae5b1ec
Hp Victus <tursunovAbd563@gmail.com>
2023-06-19 17:42:23 +0500

    screen 5v full connection 3 screen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -21,6 +21,7 @@ class _AllScreenButtonState extends State<AllScreenButton> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.blueGrey,
         elevation: 0,
         title: const Text(
